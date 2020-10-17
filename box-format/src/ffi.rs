7bf60e3ac0c9bbfb9f28056a69adf608428aa5ff
Brendan Molloy <brendan@bbqsrc.net>
2020-10-17 16:34:41 +0200

    Use cffi

--- box-format/src/ffi.rs
+++ box-format/src/ffi.rs
@@ -1,21 +1,21 @@
-use cursed::{FromForeign, ToForeign};
+use cffi::{FromForeign, ToForeign};
 
 use crate::BoxFileReader;
 use std::path::PathBuf;
 
-#[cthulhu::invoke(return_marshaler = "cursed::BoxMarshaler::<BoxFileReader>")]
+#[cffi::marshal(return_marshaler = "cffi::BoxMarshaler::<BoxFileReader>")]
 pub extern "C" fn box_file_reader_open(
-    #[marshal(cursed::PathMarshaler)] path: PathBuf,
+    #[marshal(cffi::PathMarshaler)] path: PathBuf,
 ) -> Result<Box<BoxFileReader>, Box<dyn std::error::Error>> {
     BoxFileReader::open(path)
         .map(|x| Box::new(x))
         .map_err(|err| Box::new(err) as _)
 }
 
-#[cthulhu::invoke(return_marshaler = "cursed::UnitMarshaler")]
+#[cffi::marshal(return_marshaler = "cffi::UnitMarshaler")]
 pub extern "C" fn box_file_reader_extract_all(
-    #[marshal(cursed::BoxRefMarshaler::<BoxFileReader>)] handle: &BoxFileReader,
-    #[marshal(cursed::PathMarshaler)] path: PathBuf,
+    #[marshal(cffi::BoxRefMarshaler::<BoxFileReader>)] handle: &BoxFileReader,
+    #[marshal(cffi::PathMarshaler)] path: PathBuf,
 ) -> Result<(), Box<dyn std::error::Error>> {
     handle.extract_all(path).map_err(|err| Box::new(err) as _)
 }
